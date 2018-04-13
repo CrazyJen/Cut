25ef25af3d10fc37ba602d271ca4332f829143fb
CrazyJen <Jaller@inbox.ru>
2018-04-14 02:45:35 +0300

    Парсер командной строки

--- src/Cut/CommandLineParser.java
+++ src/Cut/CommandLineParser.java
@@ -1,21 +1,62 @@
 package Cut;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class CommandLineParser {
     private String input = "", output = "";
-    private int nRange, kRange;
+    private int nRange = -1, kRange = -1;
     private boolean chars = false, words = false;
 
     public CommandLineParser(String[] args) {
         StringBuilder check = new StringBuilder(args[0]);
         for (int i = 1; i < args.length; i++) {
-            check.append(' ' + args[i]);
+            check.append(' ').append(args[i]);
+        }
+        if (!check.toString().matches("cut (-c|-w) (-o \\w+\\.txt )?(\\w+\\.txt )?(\\d+-\\d*|\\d*-\\d+)"))
+            throw new IllegalArgumentException("Wrong command line");
+
+        for (int i = 1; i < args.length; i++) {
+            switch (args[i]) {
+                case ("-c"):
+                    chars = true;
+                case ("-w"):
+                    words = true;
+                case ("-o"): {
+                    output = args[i + 1];
+                    String s = args[i + 2];
+                    if (s.matches("\\w+\\.txt")) input = s;
+                }
+                default:
+            }
         }
 
-        if ()
-            throw new IllegalArgumentException("Incorrect command line");
+        String[] range = args[args.length-1].split("-");
+        if (!range[0].equals("")) nRange = Integer.parseInt(range[0]);
+        if (!range[1].equals("")) kRange = Integer.parseInt(range[1]);
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public int getnRange() {
+        return nRange;
+    }
+
+    public int getkRange() {
+        return kRange;
+    }
+
+    public boolean isChars() {
+        return chars;
+    }
+
+    public boolean isWords() {
+        return words;
     }
 }
